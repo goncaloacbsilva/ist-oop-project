613af1a5aed738835354a60c15af0cf8ce76be63
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2021-10-26 19:03:47 +0100

    (feat): Implemented DoSaveFile method on APP layer

--- ggc/app/main/DoSaveFile.java
+++ ggc/app/main/DoSaveFile.java
@@ -2,22 +2,46 @@ package ggc.app.main;
 
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import pt.tecnico.uilib.forms.Form;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+import ggc.app.exception.FileCouldNotBeFoundException;
+import ggc.app.exception.FileOpenFailedException;
 import ggc.core.WarehouseManager;
-//FIXME import classes
+import ggc.core.exception.MissingFileAssociationException;
 
 /**
  * Save current state to file under current name (if unnamed, query for name).
  */
 class DoSaveFile extends Command<WarehouseManager> {
 
+  String filename;
+  private Form _form;
+
   /** @param receiver */
   DoSaveFile(WarehouseManager receiver) {
     super(Label.SAVE, receiver);
+    _form = new Form();
   }
 
   @Override
   public final void execute() throws CommandException {
-    //FIXME implement command and create a local Form
+    try {
+      try {
+        _receiver.save();
+      } catch (MissingFileAssociationException e) {
+        _form.addStringField("filename", Message.newSaveAs());
+        _form.parse();
+        _receiver.saveAs(_form.stringField("filename"));
+      }
+    } catch (FileNotFoundException e) {
+      throw new FileCouldNotBeFoundException(_receiver.getFilename());
+    } catch (IOException e) {
+      throw new FileOpenFailedException(_receiver.getFilename());
+    }
   }
 
 }
