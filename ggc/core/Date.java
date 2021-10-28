4aa83a71e85b32f3c08f076920860ba8b6f92153
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2021-10-28 18:36:19 +0100

    (feat): Static Date is working now

--- ggc/core/Date.java
+++ ggc/core/Date.java
@@ -9,51 +9,55 @@ import ggc.core.exception.InvalidDateValueException;
  */
 public class Date implements Serializable {
     private static final long serialVersionUID = 202109192006L;
-    private int _value; // Should be Static! But right now it cant be because of Serialization
+
+    /** Global value shared by all Date instances */
+    private static int _globalValue;
+
+    /** Local value for independent Date objects */
+    private int _localValue;
 
     /**
      * Creates a new Date object with the supplied value
      * @param value
      */
     public Date(int value) {
-        _value = value;
+        _localValue = value;
     }
 
     /**
-     * Advances a given number of days
+     * Advances the global value a given number of days
      * @param value number of days to advance
-     * @return new Date object
      * @throws InvalidDateValueException
      */
-    public Date add(int value) throws InvalidDateValueException {
+    public static void add(int value) throws InvalidDateValueException {
         if (value <= 0) {
             throw new InvalidDateValueException(value);
         }
-        return new Date(_value + value);
+        _globalValue += value;
     }
 
     /**
-     * Calculates the difference between two dates
+     * Calculates the difference between two dates (local)
      * @param date Date to compare
      * @return absolute difference between dates 
      */
     public int difference(Date date) {
-        return Math.abs(date.getValue() - _value);
+        return Math.abs(date.getValue() - _localValue);
     }
 
     /**
-     * Get Date value
-     * @return value
+     * Get Date local value
+     * @return local value
      */
     public int getValue() {
-        return _value;
+        return _localValue;
     }
 
     /**
-     * Get the current Date object
+     * Get the "Global" Date object
      * @return Date object
      */
-    public Date now() {
-        return new Date(_value);
+    public static Date now() {
+        return new Date(_globalValue);
     }
 }
